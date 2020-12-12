df9397a28ba98463753e1775f8e8ad991a9dbdd8
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2020-12-12 10:42:52 -0300

    Add files via upload

---
+++ classe/classe_bijuu.php
@@ -0,0 +1,21 @@
+<?php
+
+	class Bijuu {
+		
+		public $raca;
+		public $tipo;
+		public $cor;
+
+		public function __construct($raca,$tipo,$cor){
+			$this->raca = $raca;
+			$this->tipo = $tipo;
+			$this->cor = $cor;
+		}
+		
+		public function Caracteristicas(){
+			echo "A bijuu é uma {$this->raca} de {$this->tipo} de cor {$this->cor}.<br>";
+		}
+
+	}
+
+?>
---
+++ classe/index_bijuu.php
@@ -0,0 +1,8 @@
+<?php 
+
+	require_once '../classe/classe_bijuu.php'; 
+
+	$bijuu = new Bijuu("Raposa","Nove caudas","Amarela");
+	$bijuu->Caracteristicas();
+
+?>
