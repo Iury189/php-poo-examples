51c83760b86c9b8c7f4e6120797133db350b3bdb
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2020-12-12 09:55:33 -0300

    Add files via upload

---
+++ getter_setter/classe_veiculo.php
@@ -0,0 +1,48 @@
+<?php  
+	
+	class Veiculo {
+
+		private $modelo;
+		private $cor;
+		private $ano;
+
+		public function getModelo(){
+			return $this->modelo;
+		}
+		
+		public function setModelo($modelo){
+			$this->modelo = $modelo;
+		}
+
+		public function getCor(){
+			return $this->cor;
+		}
+
+		public function setCor($cor){
+			$this->cor = $cor;
+		}
+
+		public function getAno(){
+			return $this->ano;
+		}
+
+		public function setAno($ano){
+			$this->ano = $ano;
+		}
+
+		public function __construct($modelo,$cor,$ano){
+			$this->setModelo($modelo);
+			$this->setCor($cor);
+			$this->setAno($ano);
+		}
+
+		public function Caracteristicas(){
+			echo "Carro {$this->getModelo()} de cor {$this->getCor()} ano de lançamento {$this->getAno()}.<br>";
+		}
+
+		public function Estepe(){
+			echo "Esse carro tem pneu de estepe. <br>";
+		}
+	}
+	
+?>
---
+++ getter_setter/index_veiculo.php
@@ -0,0 +1,9 @@
+<?php  
+	
+	require_once 'classe_veiculo.php';
+	
+	$carro = new Veiculo("Tesla Model Y","Vermelha","2021");
+	$carro->Caracteristicas();
+	$carro->Estepe();
+
+?>
