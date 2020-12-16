6ee99ff636bbb2e5cbe0ae9acaca35cf8a33e399
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2020-12-16 08:30:39 -0300

    Add files via upload

--- abstract/extend_moto.php
+++ abstract/extend_moto.php
@@ -3,11 +3,11 @@
 	class Moto extends Veiculo {
 
 		public function Andar(){
-			echo 'Estou dando partida na motoclicleta. <br>';
+			echo "Estou dando partida na motoclicleta. <br>";
 		}
 
 		public function Caracteristicas(){
-			echo 'A moto é uma {$this->getModelo()} de cor {$this->getCor()} fabricada em {$this->getAno()}. <br>';
+			echo "A moto é uma {$this->getModelo()} de cor {$this->getCor()} fabricada em {$this->getAno()}. <br>";
 		}
 	}
 	
