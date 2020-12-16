f84dae9d140a51fc3599bf05d2d79df4d99d79e4
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2020-12-16 08:28:28 -0300

    Add files via upload

--- getter_setter/index_veiculo.php
+++ getter_setter/index_veiculo.php
@@ -1,6 +1,6 @@
 <?php  
 	
-	require_once '../getter_setter/classe_veiculo.php';
+	require_once 'classe_veiculo.php';
 	
 	$carro = new Veiculo("Tesla Model Y","Vermelha","2021");
 	$carro->Caracteristicas();
