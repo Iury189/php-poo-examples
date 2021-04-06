8a12c3aa1417cf58ff3cf74e5ff09c757a59d5b6
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-04-06 11:18:04 -0300

    Update classe_bijuu.php

--- classe/classe_bijuu.php
+++ classe/classe_bijuu.php
@@ -11,7 +11,7 @@
 		}
 		
 		public function Caracteristicas(){
-			echo "A bijuu é {$this->raca} de {$this->tipo} de cor {$this->cor}.<br>";
+			echo "A bijuu é {$this->raca} do tipo {$this->tipo} de cor {$this->cor}.<br>";
 		}
 
 	}
