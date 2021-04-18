f641ac0d2ae5a2e4179e0fe8c2ff7527c5fbb0b7
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-04-18 10:18:53 -0300

    Update classe_dh.php

--- polimorfismo/classe_dh.php
+++ polimorfismo/classe_dh.php
@@ -4,21 +4,13 @@
 
 		private $sword, $gun;
 
-		public function getSword(){
-			return $this->sword;
-		}
+		public function getSword() { return $this->sword; }
 
-		public function setSword($sword){
-			$this->sword = $sword;
-		}
+		public function setSword($sword) { $this->sword = $sword; }
 
-		public function getGun(){
-			return $this->gun;
-		}
+		public function getGun() { return $this->gun; }
 
-		public function setGun($gun){
-			$this->gun = $gun;
-		}
+		public function setGun($gun) { $this->gun = $gun; }
 
 		public function __construct($sword,$gun){
 			$this->setSword($sword);
