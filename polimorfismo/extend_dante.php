d1b4c211839b2fb39ad6716e63236ab71fd34f8f
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-04-18 10:20:10 -0300

    Update extend_dante.php

--- polimorfismo/extend_dante.php
+++ polimorfismo/extend_dante.php
@@ -2,22 +2,13 @@
 
 	class Dante extends DevilHunter{
 
-		public function Falar(){
-			echo "Let's rock!!! <br>";
-		}
+		public function Falar() { echo "Let's rock!!! <br>"; }
 
-		private function Armory(){
-			echo "My sword is {$this->getSword()} and my weapons are {$this->getGun()}. <br>";
-		}
+		private function Armory() { echo "My sword is {$this->getSword()} and my weapons are {$this->getGun()}. <br>"; }
 
-		protected function Legacy(){
-			echo "My Legacy. <br>";
-		}
+		protected function Legacy(){ echo "My Legacy. <br>"; }
 
-		public function View(){
-			$this->Armory();
-			$this->Legacy();
-		}
+		public function View() { $this->Armory(); $this->Legacy(); }
 	}
 
 ?>
