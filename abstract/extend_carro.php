10a9ba1dc77835a4538ae6e5992c0d51f7840ab4
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-04-18 10:29:29 -0300

    Update extend_carro.php

--- abstract/extend_carro.php
+++ abstract/extend_carro.php
@@ -2,13 +2,9 @@
 
 	class Carro extends Veiculo {
 
-		public function Andar(){
-			echo "Estou dando partida no carro. <br>";
-		}
+		public function Andar() { echo "Estou dando partida no carro. <br>"; }
 
-		public function Caracteristicas(){
-			echo "O carro é um {$this->getModelo()} de cor {$this->getCor()} fabricado em {$this->getAno()}. <br>";
-		}
+		public function Caracteristicas() { echo "O carro é um {$this->getModelo()} de cor {$this->getCor()} fabricado em {$this->getAno()}. <br>"; }
 	}
 	
 ?>
