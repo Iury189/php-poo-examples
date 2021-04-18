aa7708f8f8d2720933ac4b8280ce1e9525872b0a
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-04-18 10:27:56 -0300

    Update classe_veiculo.php

--- abstract/classe_veiculo.php
+++ abstract/classe_veiculo.php
@@ -4,35 +4,19 @@
 
 		private $modelo, $cor, $ano;
 
-		public function getModelo(){
-			return $this->modelo;
-		}
+		public function getModelo() { return $this->modelo; }
 		
-		public function setModelo($modelo){
-			$this->modelo = $modelo;
-		}
+		public function setModelo($modelo) { $this->modelo = $modelo; }
 
-		public function getCor(){
-			return $this->cor;
-		}
+		public function getCor() { return $this->cor; }
 
-		public function setCor($cor){
-			$this->cor = $cor;
-		}
+		public function setCor($cor) { $this->cor = $cor; }
 
-		public function getAno(){
-			return $this->ano;
-		}
+		public function getAno() { return $this->ano; }
 
-		public function setAno($ano){
-			$this->ano = $ano;
-		}
+		public function setAno($ano) { $this->ano = $ano; }
 
-		public function __construct($modelo,$cor,$ano){
-			$this->setModelo($modelo);
-			$this->setCor($cor);
-			$this->setAno($ano);
-		}
+		public function __construct($modelo,$cor,$ano) { $this->setModelo($modelo); $this->setCor($cor); $this->setAno($ano); }
 
 		abstract protected function Andar();
 		abstract protected function Caracteristicas();
