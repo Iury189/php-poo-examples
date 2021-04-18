8782b18cd5aa6b6fe302b2e4b0771beda87e165d
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-04-18 10:52:38 -0300

    Update classe_veiculo.php

--- getter_setter/classe_veiculo.php
+++ getter_setter/classe_veiculo.php
@@ -4,43 +4,23 @@
 
 		private $modelo, $cor, $ano;
 
-		public function getModelo(){
-			return $this->modelo;
-		}
+		public function getModelo() { return $this->modelo; }
 		
-		public function setModelo($modelo){
-			$this->modelo = $modelo;
-		}
+		public function setModelo($modelo) { $this->modelo = $modelo; }
 
-		public function getCor(){
-			return $this->cor;
-		}
+		public function getCor() { return $this->cor; }
 
-		public function setCor($cor){
-			$this->cor = $cor;
-		}
+		public function setCor($cor) { $this->cor = $cor; }
 
-		public function getAno(){
-			return $this->ano;
-		}
+		public function getAno() { return $this->ano; }
 
-		public function setAno($ano){
-			$this->ano = $ano;
-		}
+		public function setAno($ano) { $this->ano = $ano; }
 
-		public function __construct($modelo,$cor,$ano){
-			$this->setModelo($modelo);
-			$this->setCor($cor);
-			$this->setAno($ano);
-		}
+		public function __construct($modelo,$cor,$ano) { $this->setModelo($modelo); $this->setCor($cor); $this->setAno($ano); }
 
-		public function Caracteristicas(){
-			echo "Carro {$this->getModelo()} de cor {$this->getCor()} ano de lançamento {$this->getAno()}.<br>";
-		}
+		public function Caracteristicas() { echo "Carro: {$this->getModelo()}, Cor: {$this->getCor()}, Ano de fabricação: {$this->getAno()}.<br>"; }
 
-		public function Estepe(){
-			echo "Esse carro tem pneu de estepe. <br>";
-		}
+		public function Estepe() { echo "Esse carro tem pneu de estepe. <br>"; }
 	}
 	
 ?>
