409f73c8c7d634d41e6070ad132caea8227b5066
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-05-01 09:08:10 -0300

    Update index_veiculo.php

--- getter_setter/index_veiculo.php
+++ getter_setter/index_veiculo.php
@@ -2,7 +2,11 @@
 	
 	require_once 'classe_veiculo.php';
 	
-	$carro = new Veiculo("Tesla Model Y","Vermelha","2021");
+	$modelo = "Tesla Model Y";
+	$cor = "Vermelha";
+	$ano = 2021;
+	
+	$carro = new Veiculo($modelo,$cor,$ano);
 	$carro->Caracteristicas();
 	$carro->Estepe();
 
